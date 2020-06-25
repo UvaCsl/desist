ea22a0001cbe2f0efc676d74b9772b28412f51c9
raymond <r.m.padmos@uva.nl>
2020-06-25 13:05:58 +0200

    Updated submodules and added Module III and IV as submodules.

--- scripts/run_in_silico_trial.sh
+++ scripts/run_in_silico_trial.sh
@@ -38,5 +38,6 @@ cp $1 $2/config.xml
 
 sudo docker run -v `realpath $2`:/patients virtual_patient_generation $3
 sudo docker run -v /var/run/docker.sock:/var/run/docker.sock -v `realpath $2`:/patients in_silico_trial /patients/config.xml `realpath $2`
+sudo docker run -v `realpath $2`:/patients in-silico-trial-outcome
 
 sudo chown -R `whoami`:`whoami` $2
