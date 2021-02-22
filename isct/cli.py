d6a04a1bc0ed1f3975cd63d0aa65b2d241290433
Max van der Kolk <m.vanderkolk@uva.nl>
2021-02-22 09:45:46 +0100

    Remove default writing of log to disk.
    The default location `/tmp/*.log` is not always present on the systems
    and for some systems it is not desired to write the logs in the `/tmp/`
    directory. Thus, when capturing the logs is important, the output
    location should simply be provided by the user.

--- isct/cli.py
+++ isct/cli.py
@@ -1,15 +1,10 @@
 import click
 import logging
-import pathlib
 from logging.handlers import RotatingFileHandler
 
 from .cli_container import container
 from .cli_patient import patient
 from .cli_trial import trial
-
-# default path for logs
-logfile = pathlib.Path('/tmp/desist.log')
-logger = logging.getLogger(__name__)
 
 
 @click.group()
@@ -20,7 +15,6 @@ logger = logging.getLogger(__name__)
               help="Increase verbosity: shows all `DEBUG` logs.")
 @click.option('--log',
               type=click.Path(writable=True),
-              default=str(logfile),
               help="Path where log files are written to.")
 def cli(verbose, log):
     """des-ist
@@ -32,16 +26,17 @@ def cli(verbose, log):
     run, and analyse in silico trials.
     """
 
-    # Setup the basic logger with a rotating logger to rotate the logfiles
-    # every 100kB, cycling through 10 backups.
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s | %(name)s | %(levelname)s | %(message)s',
-        handlers=[
-            # The logfiles rotate every 100kB.
-            RotatingFileHandler(log, maxBytes=10000, backupCount=10)
-        ],
-    )
+    if log:
+        # Setup the basic logger with a rotating logger to rotate the logfiles
+        # every 100kB, cycling through 10 backups.
+        logging.basicConfig(
+            level=logging.INFO,
+            format='%(asctime)s | %(name)s | %(levelname)s | %(message)s',
+            handlers=[
+                # The logfiles rotate every 100kB.
+                RotatingFileHandler(log, maxBytes=100000, backupCount=10)
+            ],
+        )
 
     if verbose:
         # define stream handler to log to console
